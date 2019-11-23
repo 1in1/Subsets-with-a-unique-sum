9e0f983ca40b42e3d4c4c7a23584fd9ffd7d6c13
1in1 <49785531+1in1@users.noreply.github.com>
2019-11-23 16:56:57 +0000

    Update 201.cpp

--- 201.cpp
+++ 201.cpp
@@ -1,66 +1,48 @@
 #include <iostream>
-#include <vector>
-#include <utility>
+#include <chrono>
 typedef unsigned long long big;
 #define MAXSUM 295425
-#define SEEN 0
-#define TWICE 101
 using namespace std;
 
 int main()
 {
-	bool*** table = new bool** [2];
-	for (int i = 0; i < 2; i++) {
-		table[i] = new bool* [MAXSUM+1];
-		for (int j = 0; j <= MAXSUM; j++) {
-			table[i][j] = new bool[102];
-			for (int k = 0; k < 102; k++)
-				table[i][j][k] = false;
-		}
-	}
+  auto start = chrono::high_resolution_clock::now();
+  int* table[51];
+  for(int i=0; i<51; i++) {
+    table[i] = new int[MAXSUM+1];
+    for(int j=0; j<=MAXSUM; j++)
+      table[i][j] = 0;
+  }
+  table[0][0] = 1;
+  int size;
+  for(int k=1; k<=100; k++){  //for each element we could add
+    if(k>50){
+      size = 50;
+    } else {
+      size = k;
+    }
 
-	for (int i = 1; i <= 100; i++) {
-		table[1][i * i][i] = true;
-		table[1][i * i][SEEN] = true;
-	}
-
-	for (int i = 2; i <= 4; i++) {
-
-		//need to clean the one we're about to use
-		for (int j = 1; j <= MAXSUM; j++) {
-			table[i%2][j][SEEN] = false;
-		}
-
-
-		for (int j = 1; j <= MAXSUM; j++) { //can definitely optimise over this loop
-			if (table[(i-1)%2][j][SEEN] == true && table[(i-1)%2][j][TWICE] == false) { //there is a unique representation of the prev sum
-				for (int l = 100; l >= 1; l--) {
-
-
-					if (table[(i-1)%2][j][l] == false) { //if not already used (and within range guaranteed by MAXSUM)
-						if (table[i%2][l * l + j][SEEN] == true) { //already seen a representation
-							  table[i%2][l * l + j][TWICE] = true;
-						}
-						else { //make it same as previous but with l flagged too
-							for (int p = 0; p < 102; p++)
-								table[i%2][l * l + j][p] = table[(i - 1) % 2][j][p];
-							table[i%2][l * l + j][l] = true;
-						}
-					} else { //only add ones that are bigger than the largest one already being used in that sum
-            break;
+    for(int i=size; i>=1; --i) {  //loop over subset size
+      for(int j=MAXSUM; j>=k*k; --j) {
+        if(table[i-1][j-k*k] > 0){ //if the value we are coming from is reachable
+          if(table[i][j] == 0) { //if we haven't already seen this value
+            table[i][j] = table[i-1][j-k*k]; //mark as seen or seen twice as appropriate
+          } else { //otherwise mark as seen twice
+            table[i][j] = 2;
           }
+        }
+      }
+    }
 
-				}
-			}
-		}
+  }
+  big total = 0;
+  for(int i = 1; i<=MAXSUM; i++) {
+    if(table[50][i] == 1)
+      total += i;
+  }
+  auto stop = chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+  cout<<total<<endl;
+  cout << "Done. Took "<<duration.count()/1000<< " milliseconds total.";
 
-	}
-
-	big total = 0;
-	for (int i = 1; i <= MAXSUM; i++) {
-		if (table[0][i][SEEN] == 1 && table[0][i][TWICE] == 0) {
-			total += i;
-		}
-	}
-	cout << total;
 }
